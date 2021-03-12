8dac18a9dd7228b1165543072e8f56607be97e94
Robert Burroughs <clifdevelops@gmail.com>
2021-03-11 23:57:57 -0700

    work on the homepage that isn't finished but I don't want to lose my progress

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,40 @@ const { logoutUser } = require('../auth')
 
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
+const convertDate = (timestamp) => timestamp.toString().slice(0, 16);
+
+const grabUser = async (id) => {
+  const user = await db.User.findByPk(id);
+  return user;
+}
+
+//working toward the official homepage.
+router.get('/homepage', asyncHandler( async(req, res, next) => {
+  const stories = await db.Story.findAll();
+  const users = await db.User.findAll();
+  // const topWriters = await db.User.findAll()
+  //this is an idea to show off the writers who have written the most stories or have the most likes.
+
+  //want to find 5 most recently written stories and display them.
+  const recentStories = await db.Story.findAll({
+    limit: 5,
+    order : [
+      ['createdAt', 'asc']
+    ]
+  });
+  const randomStories = await db.Story.findAll({ 
+    limit: 5,
+    //randomize
+  })
+
+  let loggedInUser = null;
+  if (req.session.auth) {
+    loggedInUser = await db.User.findByPk(req.session.auth.userId);
+  }
+
+  res.render('homepage', {title: "ANIMEDIUM!", stories, users, recentStories, loggedInUser, convertDate, grabUser })
+}))
+
 
 /* GET home page. */
 router.get('/', asyncHandler( async(req, res, next)=> {

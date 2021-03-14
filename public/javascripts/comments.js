4ae1d09740b230b46a8d51fb8a5436ee88a0b9c5
nic <nicgauer@gmail.com>
2021-03-14 11:22:12 -0500

    fixed comment buttons when logged out, redirects to login

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -10,68 +10,71 @@ document.addEventListener('DOMContentLoaded', async () => {
     let emptyMessage = false;
     let errorMessage = false;
 
+    
 
     //Add comments ---
-    commentSubmit.addEventListener('click', async () => {
-        //grab input
-        const textArea = document.getElementById('comment__body');
-        //grab comment body from input
-        const body = textArea.value;
-        //'hidden' inputs -- we used class list to store variables from our pug file, destructured here
-        const storyId = textArea.classList[0];
-        const userId = textArea.classList[1];
+    if(commentSubmit){
+        commentSubmit.addEventListener('click', async () => {
+            //grab input
+            const textArea = document.getElementById('comment__body');
+            //grab comment body from input
+            const body = textArea.value;
+            //'hidden' inputs -- we used class list to store variables from our pug file, destructured here
+            const storyId = textArea.classList[0];
+            const userId = textArea.classList[1];
 
-        //if comment body exists, comment is posted.  If no comment body exists, an error message is rendered.
-        if(body){
-            //Future comment stores variables from our text area
-            const futureComment = {
-                body: body,
-                userId: userId,
-                storyId: storyId
-            }
-
-            //calling our post to our database
-            await fetch(`/stories/${storyId}/comments`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(futureComment),
-            })
-
-            //Checks to see if user has enabled "view comments"
-            if(commentsVisible){
-                //Checks for and removes error messages before adding new comments
-                if(emptyMessage || errorMessage){
-                    commentUL.innerHTML = '';
-                    if(emptyMessage) emptyMessage = false;
-                    if(errorMessage) errorMessage = false;
+            //if comment body exists, comment is posted.  If no comment body exists, an error message is rendered.
+            if(body){
+                //Future comment stores variables from our text area
+                const futureComment = {
+                    body: body,
+                    userId: userId,
+                    storyId: storyId
                 }
 
-                //more 'hidden' inputs from our text area -- aka class list
-                const screenName = textArea.classList[3]
-                const pictureURL = textArea.classList[2]
+                //calling our post to our database
+                await fetch(`/stories/${storyId}/comments`, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(futureComment),
+                })
 
-                commentUL.appendChild(renderComment(screenName, body, pictureURL));
+                //Checks to see if user has enabled "view comments"
+                if(commentsVisible){
+                    //Checks for and removes error messages before adding new comments
+                    if(emptyMessage || errorMessage){
+                        commentUL.innerHTML = '';
+                        if(emptyMessage) emptyMessage = false;
+                        if(errorMessage) errorMessage = false;
+                    }
 
+                    //more 'hidden' inputs from our text area -- aka class list
+                    const screenName = textArea.classList[3]
+                    const pictureURL = textArea.classList[2]
+
+                    commentUL.appendChild(renderComment(screenName, body, pictureURL));
+
+                }
+                //Resets text area and disables the comment form if comment post successful
+                textArea.value = '';
+                commentFormContainer.classList.add('hidden');
+                // commentAddButton.classList.remove('hidden');
+            }else{
+                //Displays an error message when body is empty//
+                //if statement prevents message from being rendered twice
+                if(!errorMessage){
+                    const msg = document.createElement('h3');
+                    msg.setAttribute('id', 'error-message');
+                    msg.innerText = 'Please enter a comment!';
+                    commentUL.appendChild(msg);
+                    errorMessage = true;
+                }
             }
-            //Resets text area and disables the comment form if comment post successful
-            textArea.value = '';
-            commentFormContainer.classList.add('hidden');
-            // commentAddButton.classList.remove('hidden');
-        }else{
-            //Displays an error message when body is empty//
-            //if statement prevents message from being rendered twice
-            if(!errorMessage){
-                const msg = document.createElement('h3');
-                msg.setAttribute('id', 'error-message');
-                msg.innerText = 'Please enter a comment!';
-                commentUL.appendChild(msg);
-                errorMessage = true;
-            }
-        }
-    })
+        })
+    }
 
 
     //Loads comments to list element
